6a0f25bd488e787b19c85702e5edc7673f18ed69
rkteddy <838474413@qq.com>
2020-05-20 22:55:44 +0800

    Fixed file name error

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python trainer.py --model rkccsnet --sensing-rate 0.5 --epochs 200 --batch-size 4\
+python train.py --model rkccsnet --sensing-rate 0.5 --epochs 200 --batch-size 4\
                   --block-size 32 --image-size 96 --lr 1e-3 --save-dir save_temp 
